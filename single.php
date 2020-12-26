24db48d335cfe59a165f5633d0b48e69baa816a5
Abhishek Nagekar <abhishek@nagekar.com>
2020-12-26 16:26:55 +0100

    Show tags on post pages

--- single.php
+++ single.php
@@ -11,67 +11,76 @@ get_header();
 ?>
 
 
-	<header>
-	  <h1><?php the_title(); ?></h1>
-	  <header class="title-tags">
-	    <time><?php the_time('j F, Y. \I\t \w\a\s \a l'); ?></time>
-	    <div class="tags-container">
-	      Posted under <span>
-	          
-	        </span>
-	      &nbsp;
-	    </div>
-	  </header>
+<header>
+	<h1><?php the_title(); ?></h1>
+	<header class="title-tags">
+		<time><?php the_time('j F, Y. \I\t \w\a\s \a l'); ?></time>
+		<div class="tags-container">
+			Posted under <span>
+				<?php
+				$post_tags = get_the_tags();
+				if ( $post_tags ) {
+					foreach($post_tags as $tag) {
+				?>
+						<a class="tags" href="#"><?php echo $tag->name; ?></a>
+				<?php
+					}
+				}
+				?>
+			</span>
+			&nbsp;
+		</div>
 	</header>
+</header>
 
-	<hr class="stylish" />
+<hr class="stylish" />
 
-	<main role="main">
+<main role="main">
 
-		<?php
-			while ( have_posts() ) :
-				the_post();
+	<?php
+	while ( have_posts() ) :
+		the_post();
 
-				$previous_post = get_previous_post();
-				$next_post = get_next_post();
+		$previous_post = get_previous_post();
+		$next_post = get_next_post();
 
 		?>
 
-			<article>
-				<section class="content">
-		      <?php the_content(); ?>
-		    </section>
+		<article>
+			<section class="content">
+				<?php the_content(); ?>
+			</section>
 
-    		<hr class="stylish"/>
+			<hr class="stylish"/>
 
-  		  <?php get_template_part( 'template-parts/featured' ); ?>
+			<?php get_template_part( 'template-parts/featured' ); ?>
 
-	      <hr class="stylish"/>
+			<hr class="stylish"/>
 
-	      <section class="pagination">
-	      	<?php if($previous_post): ?>
-		      	
-		      	<a class="prev" href="<?php echo get_permalink($previous_post); ?>">&laquo; <?php echo get_the_title($previous_post); ?></a>
-		      
-		      <?php 
-		    		endif; 
-		      	if($next_post): 
-		      ?>
-		      	
-		      	<a class="next" href="<?php echo get_permalink($next_post); ?>"> <?php echo get_the_title($next_post); ?> &raquo;</a>
-		      
-		      <?php
-		      	endif;
-		      ?>
-		    </section>
+			<section class="pagination">
+				<?php if($previous_post): ?>
 
-			</article>
+					<a class="prev" href="<?php echo get_permalink($previous_post); ?>">&laquo; <?php echo get_the_title($previous_post); ?></a>
+
+					<?php 
+				endif; 
+				if($next_post): 
+					?>
+
+					<a class="next" href="<?php echo get_permalink($next_post); ?>"> <?php echo get_the_title($next_post); ?> &raquo;</a>
+
+					<?php
+				endif;
+				?>
+			</section>
+
+		</article>
 
 		<?php
 			endwhile; // End of the loop.
-		?>
+			?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
-<?php
-get_footer();
+		<?php
+		get_footer();
