d57e4e874e927a01ac21b765f253bd046037f49a
Abhishek Nagekar <abhishek@nagekar.com>
2021-01-23 01:24:14 +0100

    Added extended nav options

---
+++ template-parts/search-box.php
@@ -0,0 +1,5 @@
+<form action="/" method="get" target="_blank">
+  <label for="search-input">➡️ Search for a specific term: </label>
+  <input id="search-input" name="s" type="text" placeholder="search term" name="" required>
+  <input type="submit" value="Search">
+</form>
