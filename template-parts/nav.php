267a4d8dcdf8d3e3d8c837db1d71245833373380
Abhishek Nagekar <abhishek@nagekar.com>
2021-01-23 02:01:32 +0100

    Some quick fixes (#19)

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -7,7 +7,7 @@
       <a class="nav-item" title="Handpicked word art" href='/quotes'>Quotes</a> |
       <a class="nav-item" title="Tech setup notes" href='/setup'>Setup</a> |
       <a class="nav-item" href='https://github.com/abhn'>Github</a> |
-      <span id="nav-details-toggle">More ✨</span>
+      <span class="nav-item" id="nav-details-toggle">More ✨</span>
     </summary>
 
     <?php
@@ -19,9 +19,6 @@
     <p>➡️ Subscribe via a <a href="https://en.wikipedia.org/wiki/Web_feed">feeds</a> reader: 
       <a href="/?feed=rss2">RSS</a>,
       <a href="/?feed=atom">Atom</a></p>
-
-    <?php
-      get_template_part('template-parts/email-subscription-form'); ?>
 
   </details>
 </nav>
